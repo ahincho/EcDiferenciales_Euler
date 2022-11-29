f4b75539efe6aed77a97fc46cc7a3f6dcce9776d
ahincho <ahincho@unsa.edu.pe>
2022-11-28 22:48:23 -0500

    Videos de teoria revisados

--- Main.cc
+++ Main.cc
@@ -2,6 +2,17 @@
 # include "Calculadora.h"
 # include <stdio.h>
 
+/*
+    Bibliografia:
+    - https://youtu.be/V6wLYLvqZ84
+    - https://youtu.be/PXdjSYPYLZ4
+    - https://youtu.be/jy9guAqutIM
+    - https://youtu.be/98pN37CP_Dc
+    - https://youtu.be/WolwH55rveQ
+    - https://youtu.be/bwlPJXH1ZV8
+    - https://youtu.be/ihnvDvg0SP0
+*/
+
 int main() {
     Calculadora cal;
     double x0 = 1.0f, h = 0.001f, xf = 3.0f, y0 = 3.0f, dy0 = -1.0f;
